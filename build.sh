f2b22b5fa4ac4283c98dfdb0a0ad3768c32e0b81
0TideLin <1583217330@qq.com>
2022-09-27 19:25:50 +0800

    [N] 1.增加git环境配置,忽略Build文件夹  2.修改工具路径
    Signed-off-by: 0TideLin <1583217330@qq.com>

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #
 #add export
 #
-export PACKAGES_PATH=$PWD
+export WORKSPACE=$PWD
+export PACKAGES_PATH=$PWD/edk2:$PWD/TidePkg
 export IASL_PREFIX=$PWD/TidePkg/ToolChain/acpica/
+echo WORKSPACE:$WORKSPACE
 echo PACKAGES_PATH:$PACKAGES_PATH
 echo IASL_PREFIX:$IASL_PREFIX
