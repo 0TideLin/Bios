b40128623c30e25e8592905a2a2ff9febc5a5231
0TideLin <1583217330@qq.com>
2022-10-04 16:59:59 +0800

    [Add]UsbView Application
    Signed-off-by: 0TideLin <1583217330@qq.com>

--- TidePkg/Application/UsbView/UsbView.c
+++ TidePkg/Application/UsbView/UsbView.c
@@ -8,7 +8,44 @@ UsbViewMain(
   IN EFI_SYSTEM_TABLE  *SystemTable
 )
 {
-  EFI_STATUS Status;
-  Status = EFI_SUCCESS;
+  EFI_STATUS              Status;
+  EFI_USB2_HC_PROTOCOL    *UsbHcProtocol;
+  EFI_HANDLE              *HandleBuffer;
+  UINTN                   HandleCount;
+  EFI_USB_PORT_STATUS     PortStatus;
+
+  Print(L"Start!\n");
+  Status = gBS->LocateHandleBuffer(ByProtocol, &gEfiUsb2HcProtocolGuid, NULL, &HandleCount, &HandleBuffer);
+  if(EFI_ERROR(Status))
+  {
+    DEBUG((DEBUG_INFO, "%r\n", Status));
+    return Status;
+  }
+
+  for (UINT8 Index; Index < HandleCount ; Index ++)
+  {
+    Status = gBS->HandleProtocol(HandleBuffer[Index], &gEfiUsb2HcProtocolGuid, (void**)&UsbHcProtocol);
+    if(EFI_ERROR(Status))
+    {
+      DEBUG((DEBUG_INFO, "Handle:%d:%r\n",Index, Status));
+      continue;
+    }
+
+    Print(L"MajorRevision:%x, MinRevision:%x\n",UsbHcProtocol->MajorRevision, UsbHcProtocol->MinorRevision);
+
+    for( UINT8 PortNum = 0; PortNum <= 8 ; PortNum ++)
+    {
+      Status = UsbHcProtocol->GetRootHubPortStatus(UsbHcProtocol,PortNum, &PortStatus);
+      if(EFI_ERROR(Status))
+      {
+        Print(L"PortNum%02d:%r\n", PortNum, Status);
+        if(PortNum == 0)continue;
+        break;
+      }
+      Print(L"ProtNum%02d:0x%4x\n", PortNum, PortStatus);
+    }
+  }
+  
+
   return Status;
 }
