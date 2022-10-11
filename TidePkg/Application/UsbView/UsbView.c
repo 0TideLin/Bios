4439b64b8fc312e8baca1e4e08b21d3a3b9d714a
0TideLin <1583217330@qq.com>
2022-10-11 19:32:39 +0800

    [Add]Pci枚举,找xHCi控制器
    Signed-off-by: 0TideLin <1583217330@qq.com>

--- TidePkg/Application/UsbView/UsbView.c
+++ TidePkg/Application/UsbView/UsbView.c
@@ -9,43 +9,50 @@ UsbViewMain(
 )
 {
   EFI_STATUS              Status;
-  EFI_USB2_HC_PROTOCOL    *UsbHcProtocol;
+  UINTN                   NumOfPciHandles;
+  UINTN                   SegmentNumber;
+  UINTN                   BusNumber;
+  UINTN                   DeviceNumber;
+  UINTN                   FunctionNumber;
+  EFI_PCI_IO_PROTOCOL     *PciIo;
   EFI_HANDLE              *HandleBuffer;
-  UINTN                   HandleCount;
-  EFI_USB_PORT_STATUS     PortStatus;
+  PCI_DEVICE_INDEPENDENT_REGION      PciRegion;
 
-  Print(L"Start!\n");
-  Status = gBS->LocateHandleBuffer(ByProtocol, &gEfiUsb2HcProtocolGuid, NULL, &HandleCount, &HandleBuffer);
-  if(EFI_ERROR(Status))
-  {
-    DEBUG((DEBUG_INFO, "%r\n", Status));
+  Status = gBS->LocateHandleBuffer(
+                                  ByProtocol,
+                                  &gEfiPciIoProtocolGuid,
+                                  NULL,
+                                  &NumOfPciHandles,
+                                  &HandleBuffer
+                                  );
+  if( EFI_ERROR(Status)){
+    Print(L"No get the Pciio Buffer %r\n", Status);
     return Status;
   }
-
-  for (UINT8 Index; Index < HandleCount ; Index ++)
+  for(UINTN Index = 0; Index < NumOfPciHandles; Index++)
   {
-    Status = gBS->HandleProtocol(HandleBuffer[Index], &gEfiUsb2HcProtocolGuid, (void**)&UsbHcProtocol);
-    if(EFI_ERROR(Status))
-    {
-      DEBUG((DEBUG_INFO, "Handle:%d:%r\n",Index, Status));
+    Status = gBS->HandleProtocol(
+                              HandleBuffer[Index],
+                              &gEfiPciIoProtocolGuid,
+                              (VOID**) &PciIo
+                              );
+    if( EFI_ERROR(Status)){
+      Print(L"HandleProtocol error: %r\n", Status);
       continue;
     }
+    //
+    //Get the Class code
+    //
+    PciIo->Pci.Read(PciIo, EfiPciIoWidthUint32, 0, sizeof(PciRegion)/sizeof(UINT32), &PciRegion);
 
-    Print(L"MajorRevision:%x, MinRevision:%x\n",UsbHcProtocol->MajorRevision, UsbHcProtocol->MinorRevision);
 
-    for( UINT8 PortNum = 0; PortNum <= 8 ; PortNum ++)
-    {
-      Status = UsbHcProtocol->GetRootHubPortStatus(UsbHcProtocol,PortNum, &PortStatus);
-      if(EFI_ERROR(Status))
-      {
-        Print(L"PortNum%02d:%r\n", PortNum, Status);
-        if(PortNum == 0)continue;
-        break;
-      }
-      Print(L"ProtNum%02d:0x%4x\n", PortNum, PortStatus);
+    Print(L"%x, %x, %x\n", PciRegion.ClassCode[0], PciRegion.ClassCode[1], PciRegion.ClassCode[2]); //ProIf,SubClass,BaseClass
+    if(IS_CLASS(PciRegion.ClassCode[2], PCI_SERIAL_BUS_CONTROLLER)){
+      Print(L"Vid:%04x,Did:%04x\n", PciRegion.VendorId, PciRegion.DeviceId);
+      PciIo->GetLocation(PciIo, &SegmentNumber,&BusNumber, &DeviceNumber, &FunctionNumber);
+      Print(L"B,D,F:%x,%x,%x\n",BusNumber, DeviceNumber, FunctionNumber);
     }
   }
-  
 
   return Status;
 }
--- TidePkg/Application/UsbView/UsbView.h
+++ TidePkg/Application/UsbView/UsbView.h
@@ -7,14 +7,32 @@
 #include <Protocol/Usb2HostController.h>
 #include <Protocol/UsbHostController.h>
 #include <Protocol/DevicePath.h>
+#include <Protocol/PciIo.h>
 
 #include <Library/BaseLib.h>
 #include <Library/DebugLib.h>
 #include <Library/BaseMemoryLib.h>
+#include <Library/MemoryAllocationLib.h>
 #include <Library/UefiApplicationEntryPoint.h>
 #include <Library/UefiBootServicesTableLib.h>
 #include <Library/UefiLib.h>
 
+#include <IndustryStandard/Pci22.h>
+
+#define PCI_SERIAL_BUS_CONTROLLER   0x0C  //Bus Class
+#define PCI_USB_CONTROLLER          0x03  //Usb Controller subclass
+#define PCI_UHCI                    0x00  //Pro-If
+#define	PCI_OHCI                    0x10
+#define	PCI_EHCI                    0x20
+#define PCI_XHCI                    0x30
+#define	PCI_UNSPECFIED              0x80
+#define	PCI_USB_DEVICE              0xFE
+
+#define PRO_IF_OFFSET               0x09
+#define SUB_CLASS_OFFSET            0x0A
+#define BASE_CLASS_OFFSET           0x0B
+
+#define IS_CLASS(a, c) ((a) == (c))
 
 
 
