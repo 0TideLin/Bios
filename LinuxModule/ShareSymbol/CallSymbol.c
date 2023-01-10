f97d891e15b01dc35f18cc29d86214db6f4c42f7
0TideLin <1583217330@qq.com>
2023-01-10 14:18:56 +0800

    LinuxModule 增加注释
    Signed-off-by: 0TideLin <1583217330@qq.com>

--- LinuxModule/ShareSymbol/CallSymbol.c
+++ LinuxModule/ShareSymbol/CallSymbol.c
@@ -6,21 +6,25 @@
 static int __init my_test_init(void)
 {
 	printk("my call symbol kernel module init\n");
-  TideFun1(1,2);
+  TideFun1(1,2);//ShareSymbol提供的接口
 	return 0;
 }
 
 static void __exit my_test_exit(void)
 {
 	printk("goodbye call symbol\n");
-  TideFun2(3,4);
+  TideFun2(3,4);//ShareSymbol提供的接口
 }
 
 module_init(my_test_init);
 module_exit(my_test_exit);
 
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("kylin");
+
+//
+//以下为modules信息，可通过modinfo命令查看
+//
+MODULE_AUTHOR("Tide");
 MODULE_DESCRIPTION("my call symbol kernel module");
 MODULE_ALIAS("mycallsymbol");
 
--- LinuxModule/ShareSymbol/ShareSymbol.c
+++ LinuxModule/ShareSymbol/ShareSymbol.c
@@ -4,20 +4,20 @@
 
 
 static int mytest = 100;
-module_param(mytest, int, 0644);
+module_param(mytest, int, 0644);//定义参数，在安装的时候可以向module提供参数值
 MODULE_PARM_DESC(mytest, "test for module parameter");
 
 void TideFun1(int a, int b)
 {
   printk("__FUNCTION__:%d %d\n", a, b);
 }
-EXPORT_SYMBOL_GPL(TideFun1);
+EXPORT_SYMBOL_GPL(TideFun1);//对其他module提供接口
 
 void TideFun2(int a, int b)
 {
   printk("__FUNCTION__:%d %d\n", a, b);
 }
-EXPORT_SYMBOL_GPL(TideFun2);
+EXPORT_SYMBOL_GPL(TideFun2);//对其他module提供接口
 
 static int __init my_test_init(void)
 {
